a1c057477c9e383317f32413bae60b0f6ab4341b
victormmp <victormmp@outlook.com>
2018-06-26 23:21:27 -0300

    Configure global parameters

--- src/GlobalParameters.py
+++ src/GlobalParameters.py
@@ -10,21 +10,39 @@ class GlobalParameters:
 
     R = None
     Bn = None
-    whiteNoiseVariance =  None
-    defaultPower = None
-    Gt = None
-    Gr = None
-    freq = None
-    L = None
-    ht = None
-    hr = None
-    pathLossExp = None
-    std_db = None
-    d0 = None
-    defaultPower = None
-    defaultRate = None
-    limiar_snr = None
-    limiar_snr_delta = None
+    whiteNoiseVariance = None
+    defaultPower = 0.2              # Transmission power
+    Pr = None                       # Received power available
+    Gt = 1                          # Effective transmitter antenna area
+    Gr = 1                          # Effective receiver antenna area
+    freq = 2.4e9                    # Transmission frequency
+    lamb = 3e8/freq                 # Wavelength of the transmitted signal
+    L = 1                           # Distance between transmitter and receiver
+    ht = 1
+    hr = 1
+    pathLossExp = 2
+    std_db = 0.1
+    d0 = 1
+    defaultRate = 5e6
+    limiar_snr = 30
+    limiar_snr_delta = 1
 
     def __init__(self):
         pass
+
+    def initialize(self):
+        self.getPr()
+        self.setWhiteNoiseVariance()
+
+    def getPr(self):
+        """
+        Pr is the power available at the receiver antenna
+        :return:
+        """
+        pass
+
+    def friis(self):
+        pass
+
+    def setWhiteNoiseVariance(self):
+        self.whiteNoiseVariance = self.Pr/1e4
