c95ca2d60b501b89126cc5d65f7d3cff5c9330b8
victormmp <victormmp@outlook.com>
2018-08-18 11:30:35 -0300

    add docstring for test methods and add README file

--- src/NetworkModel.py
+++ src/NetworkModel.py
@@ -9,10 +9,6 @@ from src.model.RadioModels import *
 from src.model.NetNode import *
 from collections import namedtuple
 
-# Initialize Global Parameters
-gp.initializeGlobalParameters(ZigBee)
-
-
 def getFitnessForNetwork(nodeList):
     """
     Calculate quality indicators for the current network. This can be used as a classification
@@ -21,6 +17,9 @@ def getFitnessForNetwork(nodeList):
     :param nodeList: The current network, described as a node arrange.
     :return: This method returns a namedtuple with all fitness indicators.
     """
+
+    # Initialize Global Parameters
+    gp.initializeGlobalParameters(ZigBee)
     
     Fitness = namedtuple("Fitness", ["meanValidLinks"])
     
@@ -37,6 +36,10 @@ def getSNRForLink(nodeA: Node, nodeB: Node):
     """
     This method returns the calculated SNR for two nodes. 
     """
+
+    # Initialize Global Parameters
+    gp.initializeGlobalParameters(ZigBee)
+
     link: Link = Link(nodeA, nodeB)
     print("Distance: ", link.distance)
     SNR = linkService.getSNR(linkService.shadowing(link.distance))
